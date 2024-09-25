425c8f256c9bf1f30300068182927a9148d0f6c7
moonysuper <moonxsuper@gmail.com>
2024-09-25 07:29:57 -0700

    s

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,11 +4,11 @@
 
 def element_at(my_list, idx):
     if idx < 0:
-        return (None)
+        return None
 
     length = len(my_list)
 
     if idx > length - 1:
-        return (None)
+        return None
 
     return(my_list[idx])
